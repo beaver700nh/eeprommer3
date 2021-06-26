1bf6914fd9f1da53244f9978aec746241b72199d
Anon Ymus <beaver700nh@gmail.com>
2021-06-26 16:33:23 -0400

    Added main program

---
+++ README.md
@@ -0,0 +1,2 @@
+# EEPROMmer3
+The 3rd revision of my EEPROM programmer
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <termios.h>
 #include <unistd.h>
 
-#include "test.hpp"
+#include "main.hpp"
 #include "args.hpp"
 
 #define PRINT_SEP printf("====================\n")
@@ -18,15 +18,11 @@ int main(int argc, char **argv) {
   char *fname = (char *) malloc(sizeof(char) * (20 + 1));
   strcpy(fname, "/dev/ttyACM0\0\0\0\0\0\0\0\0");
 
-  parse_args(argc, argv, args);
-  print_args(args);
-  PRINT_SEP;
-
   if (length_args(args) != 0 && args[0].type == ARG) {
     strcpy(fname, args[0].contents);
   }
 
-  printf("File: %s\n", fname);
+  //
 
   return 0;
 }
