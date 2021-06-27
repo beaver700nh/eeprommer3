c6d8267865a057b1b3c2b36919b44325a4b71347
Anon Ymus <beaver700nh@gmail.com>
2021-06-27 08:46:49 -0400

    Added menu and status bar

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,46 @@
 #include <unistd.h>
 
 #include "main.hpp"
-#include "args.hpp"
 
-#define PRINT_SEP printf("====================\n")
+IMPLEMENT_APP(MainApp)
 
-Arg args[10];
+bool MainApp::OnInit() {
+  MainFrame *win = new MainFrame("Hello, world!", wxDefaultPosition, wxSize(600, 500));
+  win->Show(true);
+  SetTopWindow(win);
 
-int main(int argc, char **argv) {
-  char *fname = (char *) malloc(sizeof(char) * (20 + 1));
-  strcpy(fname, "/dev/ttyACM0\0\0\0\0\0\0\0\0");
-
-  if (length_args(args) != 0 && args[0].type == ARG) {
-    strcpy(fname, args[0].contents);
-  }
-
-  //
-
-  return 0;
+  return true;
 }
+
+MainFrame::MainFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
+  : wxFrame((wxFrame *) nullptr, -1, title, pos, size) {
+  CreateStatusBar(1);
+  SetStatusText("Hello, world!", 0);
+
+  menu_bar = new wxMenuBar();
+
+  file_menu = new wxMenu();
+  file_menu->Append(wxID_OPEN, "Open\tCtrl-O",          "Open an existing file.");
+  file_menu->Append(wxID_SAVE, "Save\tCtrl-S",          "Save to currently open file.");
+  file_menu->Append(ID_SAVEAS, "Save As\tCtrl-Shift-S", "Save this file under another name.");
+
+  tools_menu = new wxMenu();
+  tools_menu->Append(ID_READ,   "Read\tAlt-R",       "Read data from the EEPROM.");
+  tools_menu->Append(ID_WRITE,  "Write\tAlt-W",      "Write data to the EEPROM.");
+  tools_menu->Append(ID_VECTOR, "Set Vector\tAlt-V", "Set a 6502 jump vector.");
+
+  actions_menu = new wxMenu();
+  actions_menu->Append(wxID_ABOUT, "About\tF2",    "Shows information about eeprommer3.");
+  actions_menu->Append(ID_HELP,    "Help\tF1",     "Shows a help screen.");
+  actions_menu->Append(wxID_EXIT,  "Quit\tCtrl-Q", "Quit eeprommer3.");
+
+  menu_bar->Append(file_menu,    "File");
+  menu_bar->Append(tools_menu,   "Tools");
+  menu_bar->Append(actions_menu, "Actions");
+
+  SetMenuBar(menu_bar);
+}
+
+BEGIN_EVENT_TABLE(MainFrame, wxFrame)
+EVT_MENU(wxID_EXIT, [=](wxCommandEvent &){ Close(false); })
+END_EVENT_TABLE()
--- src/main.hpp
+++ src/main.hpp
@@ -1,6 +1,38 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+#ifdef WX_PRECOMP
+#  include <wx/wxprec.h>
+#else
+#  include <wx/wx.h>
+#endif
 
+#undef __GXX_ABI_VERSION
+#define __GXX_ABI_VERSION 1013
+
+class MainApp : public wxApp {
+public:
+  virtual bool OnInit();
+};
+
+class MainFrame : public wxFrame {
+public:
+  MainFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+
+  DECLARE_EVENT_TABLE()
+
+private:
+  wxMenuBar *menu_bar;
+  wxMenu *file_menu, *tools_menu, *actions_menu;
+};
+
+int \
+  ID_SAVEAS = wxNewId(),
+  ID_READ   = wxNewId(),
+  ID_WRITE  = wxNewId(),
+  ID_VECTOR = wxNewId(),
+  ID_HELP   = wxNewId();
+
+DECLARE_APP(MainApp)
 
 #endif
